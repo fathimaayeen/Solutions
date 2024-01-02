57bf563027cc4ad467761d4801922f25ae24c70c
FathimaAyeen <93316023+fathimaayeen@users.noreply.github.com>
2024-01-02 15:42:30 +0530

    Update Todo.java

--- TODO/Todo.java
+++ TODO/Todo.java
@@ -1,79 +1,47 @@
-
 package com.example.todo.model;
 
-
 public class Todo {
+    private int id;
+    private String todo;
+    private String priority;
+    private String status;
 
-
-    int id;
-    String todo;
-    String status;
-    String priority;
-
-    public Todo(int id, String todo,String status, String priority) {
+    public Todo(int id, String todo, String priority, String status) {
         this.id = id;
         this.todo = todo;
-        this.status = status;
         this.priority = priority;
-
+        this.status = status;
     }
-
 
     public int getId() {
         return id;
     }
 
-
     public void setId(int id) {
         this.id = id;
     }
-
 
     public String getTodo() {
         return todo;
     }
 
-
     public void setTodo(String todo) {
         this.todo = todo;
-    }
-
-     public String getStatus() {
-        return status;
-    }
-
-
-    public void setStatus(String status) {
-        this.status = status;
     }
 
     public String getPriority() {
         return priority;
     }
 
-
     public void setPriority(String priority) {
         this.priority = priority;
     }
 
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
